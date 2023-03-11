0983cadeff4334c36c975cb5caea9e3d96b471f6
dhyey <dhyeylalseta@outlook.com>
2023-03-11 13:16:41 -0700

    [server] request logging and temporal ⏰

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,8 @@
-import { initTRPC } from "@trpc/server";
-
-export const t = initTRPC.create();
+import { procedure, t } from "./trpc";
 
 export const appRouter = t.router({
   // TODO(dhyxy): need to setup data validation library (e.g. zod, typia, etc.)
-  helloWorld: t.procedure
+  helloWorld: procedure
     .input((data: unknown) => (typeof data === "string" ? data : "world"))
     .query((req) => `hello ${req.input}`),
 });
