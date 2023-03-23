93145c1a366b135597edaebecd6288cf83448d1d
brian-ngyn <brian.nguy1en@gmail.com>
2023-03-23 16:27:22 -0600

    fix linting

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,4 +1,7 @@
-import { render, screen } from "@testing-library/react"; import { describe, expect, it } from "vitest"; import mockTrpc from "../__mocks__/trpc.mock";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+import mockTrpc from "../__mocks__/trpc.mock";
 import App from "../App";
 import { partialMocked } from "./util";
 
